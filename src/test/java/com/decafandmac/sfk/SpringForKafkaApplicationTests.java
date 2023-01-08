62df0e66e2f96c33f783797e527585d1bf0962e6
decafandmac <eipeti.s2@gmail.com>
2023-01-08 23:15:23 +0900

    [SFK-1] first commit

---
+++ src/test/java/com/decafandmac/sfk/SpringForKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decafandmac.sfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
